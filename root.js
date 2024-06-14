af4671a66a562e7aaebdc224d13d5db4062b3039
PigTu <imblackpig@gmail.com>
2024-06-14 22:36:31 +0400

    reupload test

--- root.js
+++ root.js
@@ -1,5 +1,1 @@
 "use strict";document.getElementById("md").addEventListener("mousedown",e=>e.stopPropagation());document.getElementById("m").addEventListener("mousedown",()=>document.getElementById("m").style.display="none");document.getElementById("p-c").addEventListener("mousedown",()=>document.getElementById("m").style.display="flex");const mlm=document.getElementById("mlm");mlm.textContent=atob("cGlnQHZpZHR1LnJ1");mlm.href=atob("bWFpbHRvOg==")+mlm.textContent;const mla=document.getElementById("mla");mla.textContent=atob("aW12aWR0dUBwcm90b24ubWU=");mla.href=atob("bWFpbHRvOg==")+mla.textContent;
-function copyEva() {
-    navigator.clipboard.writeText('evatable.ru');
-    alert('Server IP is now in your clipboard.');
-}
